7dcd830bc91824990ef001308f4ec44a4236fe37
Vladirien <vladirien@gmail.com>
2015-11-19 15:24:15 +0100

    La lecture et Les paquets

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 
+import glob,random
 
-ListeInverse={};
+def Main():
+    #lecture de tous les fichiers et constitution des paquets 
+    ListesInverse={}
+    File2Author = {}
+    File2Paquet = {}
+    for filename in glob.iglob('./txt/*.txt'):
+        author = filename.split("\\")[1]
+        author = author.split(".txt")[0]
+        author = author[:-1]
+        File2Author[filename]=author
+        File2Paquet[filename]=random.randint(1,11)
+        content=LireFichier(filename)
+        tokens = Tokenize(content)
+        
+        
+    print File2Paquet
 
-
-def LireFichier(name):
-	res=""
-	return res
+def LireFichier(filename):
+    res=""
+    for line in open(filename):
+        res = res + line
+    return res
 
 def Tokenize(content):
-	res=[]
-	return res
+    res=[]
+    return res
 
 def StopList(tokens):
-	res=[]
-	return res
+    res=[]
+    return res
 
 def TermFrequency(tokens):
-	res={}
-	return res
+    res={}
+    return res
 
 def InverseDocumentFrequency(keyword):
-	res=0
-	return res
+    res=0
+    return res
 
 def Ponderation(tokens):
-	res={}
-	return res
+    res={}
+    return res
 
 
+if __name__ == '__main__':
+    Main()
 
-
-		
+        
