bffdd93ad9ba26df9e11a665775b3904265e1f38
Morgan Bouculat <morgan.bouculat@gmail.com>
2015-11-19 15:23:45 +0100

    Tokenize OK

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-
+# coding: utf8
+import re
 
 ListeInverse={};
 
-
 def LireFichier(name):
-	res=""
-	return res
+    res=""
+    return res
 
+content = u"àéèêâôùû Test de découpage du texte lu, avec même des virgules et des caractères spéciaux et tout maggle ! TKT; c'est trop cool..."
+
+# Découpage du fichier lu en tableau
 def Tokenize(content):
-	res=[]
-	return res
+    content = content.lower()
+    content = re.sub(r'[^a-zA-Z\xe0\xe9\xe8\xea\xe2\xf4\xf9\xfb\' ]',r'',content)    
+    res = content.split()
+    return res  
 
 def StopList(tokens):
-	res=[]
-	return res
+    res=[]
+    return res
 
 def TermFrequency(tokens):
-	res={}
-	return res
+    res={}
+    return res
 
 def InverseDocumentFrequency(keyword):
-	res=0
-	return res
+    res=0
+    return res
 
 def Ponderation(tokens):
-	res={}
-	return res
-
-
-
-
+    res={}
+    return res
 		
