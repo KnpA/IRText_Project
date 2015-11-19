846326c3071c4d7719b2ea8ac8c1ea8204aacdc9
Vladirien <vladirien@gmail.com>
2015-11-19 14:46:11 +0100

    Init main

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 
 
+ListeInverse={};
+
+
+def LireFichier(name):
+	res=""
+	return res
+
+def Tokenize(content):
+	res=[]
+	return res
+
+def StopList(tokens):
+	res=[]
+	return res
+
+def TermFrequency(tokens):
+	res={}
+	return res
+
+def InverseDocumentFrequency(keyword):
+	res=0
+	return res
+
+def Ponderation(tokens):
+	res={}
+	return res
+
+
 
 
 		
